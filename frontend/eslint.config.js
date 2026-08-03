2b7e78033785726ed97b1aa5625f18c1b4c5d400
MCesarczyk <michal.cesarczyk@gmail.com>
2026-08-03 14:14:20 +0200

    ci: gate frontend pipeline with lint stage

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -18,6 +18,7 @@ export default defineConfig(
 	svelte.configs.recommended,
 	prettier,
 	svelte.configs.prettier,
+	storybook.configs['flat/recommended'],
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 		rules: {
